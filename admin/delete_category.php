f68f209a0054c56918b34114d761ab0dda6d39cd
NoorRaisa <gazinoor01@gmail.com>
2024-08-09 20:37:34 +0600

    Order management in admin side

--- admin/delete_category.php
+++ admin/delete_category.php
@@ -6,6 +6,5 @@ session_start();
 
 // sending query
 mysqli_query($db,"DELETE FROM res_category WHERE c_id = '".$_GET['cat_del']."'");
-header("location:add_category.php");  
-
+header("location:add_category.php");
 ?>
---
+++ admin/view_order.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("../connection/connect.php");
+error_reporting(0);
+session_start();
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/helper.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   
+<script language="javascript" type="text/javascript">
+var popUpWin=0;
+function popUpWindow(URLStr)
+{
+ if(popUpWin)
+{
+    if(!popUpWin.closed) popUpWin.close();
+}
+popUpWin = open(URLStr,'popUpWin', 'width='+1000+',height='+600+'');
+}
+
+</script>
+</head>
+
+<body class="fix-header fix-sidebar">
+    <!-- Main wrapper  -->
+    <div id="main-wrapper">
+        <!-- header header  -->
+        <div class="header">
+            <nav class="navbar top-navbar navbar-expand-md navbar-light">
+                <div class="navbar-collapse">
+                    <!-- toggle and nav items -->
+                    <ul class="navbar-nav mr-auto mt-md-0">
+                        <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted  " href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
+                        <li class="nav-item m-l-10"> <a class="nav-link sidebartoggler hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
+                     
+                    </ul>
+                    <ul class="navbar-nav my-lg-0">                     
+                        <!-- Profile -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="images/1.png" alt="user" class="profile-pic" /></a>
+                            <div class="dropdown-menu dropdown-menu-right animated zoomIn">
+                                <ul class="dropdown-user">
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <!-- End header header -->
+        <!-- Left Sidebar  -->
+        <div class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                   <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                        <li class="nav-label">Home</li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Dashboard</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="dashboard.php">Dashboard</a></li>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-label">Log</li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false">  <span><i class="fa fa-user f-s-20 "></i></span><span class="hide-menu">Users</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="allusers.php">All Users</a></li>
+								<li><a href="add_users.php">Add Users</a></li>
+								
+                               
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-archive f-s-20 color-warning"></i><span class="hide-menu">Store</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="allrestraunt.php">All Stores</a></li>
+								<li><a href="add_category.php">Add Category</a></li>
+                                <li><a href="add_restraunt.php">Add Restaurant</a></li>
+                                
+                            </ul>
+                        </li>
+                      <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_menu.php">All Menues</a></li>
+								<li><a href="add_menu.php">Add Menu</a></li>
+                              
+                                
+                            </ul>
+                        </li>
+						 <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="hide-menu">Orders</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_orders.php">All Orders</a></li>
+								  
+                            </ul>
+                        </li>
+                         
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </div>
+        <!-- End Left Sidebar  -->
+        <!-- Page wrapper  -->
+        <div class="page-wrapper">
+            <!-- Bread crumb -->
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">Dashboard</h3> </div>
+                
+            </div>
+            <!-- End Bread crumb -->
+            <!-- Container fluid  -->
+            <div class="container-fluid">
+                <!-- Start Page Content -->
+                <div class="row">
+                    <div class="col-12">
+                        
+                       
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">View user Orders</h4>
+                             
+                                <div class="table-responsive m-t-20">
+                                    <table id="myTable" class="table table-bordered table-striped">
+                                       
+                                        <tbody>
+                                           <?php
+											$sql="SELECT users.*, users_orders.* FROM users INNER JOIN users_orders ON users.u_id=users_orders.u_id where o_id='".$_GET['user_upd']."'";
+												$query=mysqli_query($db,$sql);
+												$rows=mysqli_fetch_array($query);			
+												?>
+											
+											<tr>
+													<td><strong>Username:</strong></td>
+												    <td><center><?php echo $rows['username']; ?></center></td>
+													   <td><center>
+													   <a href="javascript:void(0);" onClick="popUpWindow('order_update.php?form_id=<?php echo $rows['o_id'];?>');" title="Update order">
+															 <button type="button" class="btn btn-primary">Take Action</button></a>
+															 </center>
+											 </td>
+												  
+																																					
+											</tr>	
+									
+											<tr>
+													<td><strong>Quantity:</strong></td>
+												    <td><center><?php echo $rows['quantity']; ?></center></td>
+													  
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Price:</strong></td>
+												    <td><center>BDT <?php echo $rows['price']; ?></center></td>
+													   
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Address:</strong></td>
+												    <td><center><?php echo $rows['address']; ?></center></td>
+													  
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Date:</strong></td>
+												     <td><center><?php echo $rows['date']; ?></center></td>
+													  
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Status:</strong></td>
+													<?php 
+																			$status=$rows['status'];
+																			if($status=="" or $status=="NULL")
+																			{
+																			?>
+																			<td> <center><button type="button" class="btn btn-info" style="font-weight:bold;">Dispatch</button></center></td>
+																		   <?php 
+																			  }
+																			   if($status=="in process")
+																			 { ?>
+																			<td>   <center><button type="button" class="btn btn-warning">On the Way!</button></center></td> 
+																			<?php
+																				}
+																			if($status=="closed")
+																				{
+																			?>
+																			<td>  <center><button type="button" class="btn btn-success" >Delivered</button></center></td> 
+																			<?php 
+																			} 
+																			?>
+																			<?php
+																			if($status=="rejected")
+																				{
+																			?>
+																			<td>  <center><button type="button" class="btn btn-danger"> <i class="fa fa-close"></i>Cancelled</button> </center></td> 
+																			<?php 
+																			} 
+																			?>	   																							
+											</tr>
+                                           
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+						 </div>
+                      
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End PAge Content -->
+            </div>
+            <!-- End Container fluid  -->
+			
+        </div>
+        <!-- End Page wrapper  -->
+    </div>
+    <!-- End Wrapper -->
+    <!-- All Jquery -->
+    <script src="js/lib/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="js/lib/bootstrap/js/popper.min.js"></script>
+    <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
+
+
+    <script src="js/lib/datatables/datatables.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
+    <script src="js/lib/datatables/cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+    <script src="js/lib/datatables/datatables-init.js"></script>
+</body>
+
+</html>
