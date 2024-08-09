5899c4ced59d00b413cd0543b9fc614273eabdd8
NoorRaisa <gazinoor01@gmail.com>
2024-08-09 21:26:24 +0600

    Update delete_menu.php

--- admin/delete_menu.php
+++ admin/delete_menu.php
@@ -6,6 +6,6 @@ session_start();
 
 // sending query
 mysqli_query($db,"DELETE FROM dishes WHERE d_id = '".$_GET['menu_del']."'");
-header("location:all_menu.php");  
+header("location:all_menu.php");
 
 ?>
